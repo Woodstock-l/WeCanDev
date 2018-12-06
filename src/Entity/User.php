d3a284e5bdaeb3e4e29ddb36af04d513c7e6324e
cl85 <cecile.lefevre85@gmail.com>
2018-12-06 14:41:15 +0100

    projet de base avec modifications

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -21,18 +21,6 @@ class User extends BaseUser
     protected $id;
 
     /**
-     * @var ?\Doctrine\Common\Collections\ArrayCollection
-     * @ORM\OneToMany(targetEntity="App\Entity\Article", mappedBy="user")
-     * @ORM\OrderBy({"id" = "DESC"})
-     */
-    private $articles;
-
-    /**
-     * @ORM\OneToMany(targetEntity="App\Entity\ArticleFollow", mappedBy="user", orphanRemoval=true)
-     */
-    private $followedArticles;
-
-    /**
      * @var null|string
      * @ORM\Column(type="string", nullable=true)
      */
@@ -40,65 +28,7 @@ class User extends BaseUser
 
     public function __construct()
     {
-        $this->articles = new \Doctrine\Common\Collections\ArrayCollection;
         parent::__construct();
-        $this->followedArticles = new ArrayCollection();
-        // your own logic
-    }
-
-    /**
-     * Get the value of articles
-     *
-     * @return  ?\Doctrine\Common\Collections\ArrayCollection
-     */ 
-    public function getArticles()
-    {
-        return $this->articles;
-    }
-
-    /**
-     * Set the value of articles
-     *
-     * @param  ?\Doctrine\Common\Collections\ArrayCollection  $articles
-     *
-     * @return  self
-     */ 
-    public function setArticles(?\Doctrine\Common\Collections\ArrayCollection $articles)
-    {
-        $this->articles = $articles;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|ArticleFollow[]
-     */
-    public function getFollowedArticles(): Collection
-    {
-        return $this->followedArticles;
-    }
-
-    public function addFollowedArticle(ArticleFollow $followedArticle): self
-    {
-        if (!$this->followedArticles->contains($followedArticle)) {
-            $this->followedArticles[] = $followedArticle;
-            $followedArticle->setUser($this);
-        }
-
-        return $this;
-    }
-
-    public function removeFollowedArticle(ArticleFollow $followedArticle): self
-    {
-        if ($this->followedArticles->contains($followedArticle)) {
-            $this->followedArticles->removeElement($followedArticle);
-            // set the owning side to null (unless already changed)
-            if ($followedArticle->getUser() === $this) {
-                $followedArticle->setUser(null);
-            }
-        }
-
-        return $this;
     }
 
     /**
